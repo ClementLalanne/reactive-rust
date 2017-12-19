c92595ab2934bbf783a23d225d506e0978fa586c
Joseph Marotte <joseph.marotte@ens.fr>
2017-12-19 16:06:54 +0100

    debug du join

--- src/process.rs
+++ src/process.rs
@@ -4,11 +4,11 @@ use std::rc::Rc;
 use std::cell::Cell;
 
 /// The implementation of the trait Process.
-pub trait Process: 'static{
+pub trait Process: 'static + Sized {
     ///Type value which is the return type of a process
     type Value;
 
-    ///Method call which execute the process in the gimen runtime and executes the continuation C on the return of the process.
+    ///Method call which execute the process in the given runtime and executes the continuation C on the return of the process.
     fn call<C>(self, runtime: &mut Runtime, next: C) where C: Continuation<Self::Value>;
 
     ///Method map which transforms the process in a new process which applies the function map to the result of the initial process.
@@ -33,6 +33,7 @@ pub trait Process: 'static{
         }
     }
 
+
     ///Method and_then equivalent to map and then flatten.
     fn and_then<F, V2>(self, map: F) -> Flatten<Map<Self, F>> where Self: Sized, F: FnOnce(Self::Value) -> V2 + 'static {
         self.map(map).flatten()
@@ -48,7 +49,7 @@ pub trait Process: 'static{
 }
 
 /// A process that can be executed multiple times, modifying its environement each time.
-pub trait ProcessMut: Process {
+/*pub trait ProcessMut: Process {
     /// Executes the mutable process in the runtime, then calls `next` with the process and the
     /// process's return value.
     fn call_mut<C>(self, runtime: &mut Runtime, next: C) where Self: Sized, C: Continuation<(Self, Self::Value)>;
@@ -73,18 +74,16 @@ pub fn execute_process<P>(p: P) -> P::Value where P:Process {
         });
     runtime.execute();
     ref_1_return.take().unwrap()
+}*/
+
+/// Implementation of the structure needed for the function value.
+pub struct Value<V> {
+    value: V,
 }
 
 ///Function value that creates a process that returns the value v.
 pub fn value<V>(v: V) -> Value<V>{
     Value::new(v)
-}
-
-
-
-/// Implementation of the structure needed for the function value.
-pub struct Value<V> {
-    value: V,
 }
 
 impl<V> Value<V> {
@@ -103,11 +102,11 @@ impl<V> Process for Value<V> where V : 'static {
     }
 }
 
-impl<V> ProcessMut for Value<V> where V : 'static{
+/*impl<V> ProcessMut for Value<V> where V : 'static{
     fn call_mut<C>(self, runtime: &mut Runtime, next: C) where Self: Sized, C: Continuation<(Self, Self::Value)>{
         next.call(runtime, (self, self.value))
     }
-}
+}*/
 
 /// Implementation of the structure needed for the map method.
 pub struct Map<P, F> {
@@ -127,7 +126,7 @@ impl<P, F, Y> Process for Map<P, F> where P: Process, F: FnOnce(P::Value) -> Y +
     }
 }
 
-impl<P,F,Y> ProcessMut for Map<P, F> where P: Process, F: FnOnce(P::Value) -> Y + 'static {
+/*impl<P,F,Y> ProcessMut for Map<P, F> where P: Process, F: FnOnce(P::Value) -> Y + 'static {
     fn call_mut<C>(self, runtime: &mut Runtime, next: C) where Self: Sized, C: Continuation<(Self, Self::Value)> {
         let f = self.map;
         self.process.call(runtime,
@@ -135,7 +134,7 @@ impl<P,F,Y> ProcessMut for Map<P, F> where P: Process, F: FnOnce(P::Value) -> Y 
                 next.call(runtime2, (self, f(value)))
         })
     }
-}
+}*/
 
 /// Implementation of the structure needed for the pause method.
 pub struct Pause<P> {
@@ -154,6 +153,7 @@ impl<P> Process for Pause<P> where P: Process{
     }
 }
 
+/*
 /*impl<P> ProcessMut for Pause<P> where P: ProcessMut{
     fn call_mut<C>(self, runtime: &mut Runtime, next: C) where Self: Sized, C: Continuation<(Self, Self::Value)> {
         runtime.on_next_instant(
@@ -163,7 +163,7 @@ impl<P> Process for Pause<P> where P: Process{
         )
     }
 }*/
-
+*/
 /// Implementation of the structure needed for the flatten method.
 pub struct Flatten<P> {
     process: P,
@@ -183,16 +183,16 @@ impl<P> Process for Flatten<P> where P: Process, P::Value: Process {
 
 /// Implementation of the structure needed for the join method.
 struct JoinPoint<P1, P2> where P1: Process, P2: Process{
-    return1: Box<Cell<Option<P1::Value>>>,
-    return2: Box<Cell<Option<P2::Value>>>,
+    return1: Cell<Option<P1::Value>>,
+    return2: Cell<Option<P2::Value>>,
     continuation: Box<Continuation<(P1::Value, P2::Value)>>,
 }
 
 impl<P1, P2> JoinPoint<P1, P2> where P1: Process, P2: Process{
     pub fn new(c: Box<Continuation<(P1::Value, P2::Value)>>) -> Self {
-        JoinPoint{
-            return1: Box::new(Cell::new(None)),
-            return2: Box::new(Cell::new(None)),
+        JoinPoint {
+            return1: Cell::new(None),
+            return2: Cell::new(None),
             continuation: c,
         }
     }
@@ -203,31 +203,40 @@ pub struct Join<P1, P2>{
     process2: P2,
 }
 
-impl<P1, P2> Process for Join<P1, P2> where P1: Process, P2: Process{
+impl <P1, P2> Process for Join<P1, P2> where P1: Process, P2: Process {
     type Value = (P1::Value, P2::Value);
 
-    fn call<C>(self, runtime: &mut Runtime, next: C) where C: Continuation<(P1::Value, P2::Value)>{
-        let join_point_1 = Rc::new(JoinPoint::new(Box<next>));
+    fn call<C>(self, runtime: &mut Runtime, next: C) where C: Continuation<(P1::Value, P2::Value)> {
+        let join_point_1 : Rc<JoinPoint<P1, P2>> = Rc::new(JoinPoint::new(Box::new(next)));
         let join_point_2 = join_point_1.clone();
+
         self.process1.call(
             runtime,
             move |runtime2: &mut Runtime, v1: P1::Value|{
-                join_point_1.return1.set(Some(v1));
                 if let Some(v2) = join_point_1.return2.take() {
-                    join_point_1.continuation.call(runtime2, (v1, v2))
-                };
-            });
+                    if let Ok(join_point_1) = Rc::try_unwrap(join_point_1) {
+                        join_point_1.continuation.call_box(runtime2, (v1, v2));
+                    }
+                } else {
+                    join_point_1.return1.set(Some(v1));
+                }
+        });
+
         self.process2.call(
             runtime,
             move |runtime2: &mut Runtime, v2: P2::Value|{
-                join_point_2.return2.set(Some(v2));
                 if let Some(v1) = join_point_2.return1.take() {
-                    join_point_2.continuation.call(runtime2, (v1, v2))
-                };
+                    if let Ok(join_point_2) = Rc::try_unwrap(join_point_2) {
+                        join_point_2.continuation.call_box(runtime2, (v1, v2));
+                    }
+                } else {
+                    join_point_2.return2.set(Some(v2));
+                }
             });
     }
 }
 
+/*
 /// Implementation of the structure needed for the while method.
 
 /// Indicates if a loop is finished.
@@ -235,7 +244,7 @@ pub enum LoopStatus<V> { Continue, Exit(V) }
 
 pub struct While<P>{
     process: P,
-}
+}*/
 
 /*impl<P, V> Process for While<P> where P: ProcessMut, P::Value: LoopStatus<V>{
     type Value = V;
