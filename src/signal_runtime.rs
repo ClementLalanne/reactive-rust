024073a2caa12f33793e4ad248f06eceb17f04dd
ClementLalanne <clement.lalanne@ens.fr>
2017-12-21 22:35:44 +0100

    some stuff added

--- src/signal_runtime.rs
+++ src/signal_runtime.rs
@@ -57,8 +57,13 @@ impl SignalRuntimeRef {
 
     /// Calls `c` at the first cycle where the signal is present.
     fn on_signal<C>(self, runtime: &mut Runtime, c: C) where C: Continuation<()> {
-        unimplemented!() // TODO
+        if *self.runtime.is_emited.borrow() {
+            c.call(runtime, ());
+        } else {
+            self.runtime.await_immediate.borrow_mut().push(Box::new(c));
+        }
     }
+
 
     // TODO: add other methods when needed.
 }
@@ -71,14 +76,26 @@ pub trait Signal {
     /// Returns a process that waits for the next emission of the signal, current instant
     /// included.
     fn await_immediate(self) -> AwaitImmediate where Self: Sized {
-      unimplemented!() // TODO
+        AwaitImmediate {
+            signal_runtime_ref : self.runtime()
+        }
+    }
+
+    fn await(self) -> Await where Self: Sized {
+        Await {
+            signal_runtime_ref : self.runtime()
+        }
+    }
+
+    fn present<C1, C2>(self, c1: C1, c2: C2) -> Present<C1, C2>  where C1: Continuation<()>, C2: Continuation<()>, Self: Sized{
+        unimplemented!() // TODO
     }
 
     // TODO: add other methods if needed.
 }
 
 struct AwaitImmediate {
-    // TODO
+    signal_runtime_ref : SignalRuntimeRef
 }
 
 /*impl Process for AwaitImmediate {
@@ -86,5 +103,31 @@ struct AwaitImmediate {
 }
 
 impl ProcessMut for AwaitImmediate {
+    // TODO
+}*/
+
+struct Await {
+    signal_runtime_ref : SignalRuntimeRef
+}
+
+/*impl Process for Await {
+    // TODO
+}
+
+impl ProcessMut for Await {
+    // TODO
+}*/
+
+struct Present<C1, C2> where C1: Continuation<()>, C2: Continuation<()> {
+    signal_runtime_ref : SignalRuntimeRef,
+    c1 : C1,
+    c2 : C2,
+}
+
+/*impl Process for Present {
+    // TODO
+}
+
+impl ProcessMut for Present {
     // TODO
 }*/
